d2dea5674f0aa153553f8d6f55b2a11355180875
Noman Ali <nomi.rana2020@gmail.com>
2022-10-09 18:12:28 +0500

    day 2 Completed

---
+++ lib/HomePage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalogue App"),
+      ),
+      body: Center(
+        child: Container(
+          child: const Text("Catelogue App"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Material(
-        child: Center(
-          child: Container(
-            child: const Text("Catelogue App"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
