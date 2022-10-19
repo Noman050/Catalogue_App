fb9bca009b2f2fcf34d315c7fa3f5a5b5fad6a7e
Noman Ali <107186063+Noman050@users.noreply.github.com>
2022-10-19 13:49:58 +0500

    new change

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:catalogue/utils/routes.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String name = "";
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,9 +23,9 @@ class LoginScreen extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            const Text(
-              "Welcome! Login To Continue",
-              style: TextStyle(
+            Text(
+              "Welcome $name! Login To Continue",
+              style: const TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
@@ -39,6 +45,11 @@ class LoginScreen extends StatelessWidget {
                       hintText: "Enter Username",
                       labelText: "Username",
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        name = value;
+                      });
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -50,20 +61,42 @@ class LoginScreen extends StatelessWidget {
                   const SizedBox(
                     height: 40,
                   ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: const StadiumBorder(),
-                      minimumSize: const Size(150, 40),
-                    ),
-                    onPressed: () {
-                      Navigator.of(context)
-                          .pushReplacementNamed(MyRoutes.homeRoute);
+                  InkWell(
+                    onTap: (){
+                       Navigator.of(context).pushReplacementNamed(MyRoutes.homeRoute);
                     },
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(fontSize: 18),
+                    child: Container(
+                      width: 150,
+                      height: 40,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
                     ),
                   ),
+                  // ElevatedButton(
+                  //   style: ElevatedButton.styleFrom(
+                  //     shape: const StadiumBorder(),
+                  //     minimumSize: const Size(150, 40),
+                  //   ),
+                  //   onPressed: () {
+                  //     Navigator.of(context)
+                  //         .pushReplacementNamed(MyRoutes.homeRoute);
+                  //   },
+                  //   child: const Text(
+                  //     "Login",
+                  //     style: TextStyle(fontSize: 18),
+                  //   ),
+                  // ),
                 ],
               ),
             ),
